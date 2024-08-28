736671c7c8d0728d29e9c7687da42377e4e6126c
Pablolh02 <pablolh.1912@gmail.com>
2024-08-27 20:47:58 -0600

    subir portafolio

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+function demoDisplay() {
+    document.getElementById("presentacion__resultado__vacio").style.display = "none";
+  }
+  
+  function demoVisibility() {
+    document.getElementById("presentacion__texto__dos").style.visibility = "hidden";
+  }
